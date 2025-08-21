d9c8284d21a2729ada06f208023e9bb02c7b39f1
上帝的梦 <1597691532@qq.com>
2025-08-21 16:46:12 +0800

    1

--- application.12055.js
+++ application.12055.js
@@ -14,7 +14,7 @@ System.register([], function (_export, _context) {
       _export("Application", Application = /*#__PURE__*/function () {
         function Application() {
           _classCallCheck(this, Application);
-          this.settingsPath = 'src/settings.9b6e9.json';
+          this.settingsPath = 'src/settings.d538e.json';
           this.showFPS = false;
         }
         _createClass(Application, [{
